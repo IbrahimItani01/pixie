5b96eca2f945f6b13788c024d1c814b78e5c70fa
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-07 12:49:11 +0300

    design hero section home page

--- app/(landing)/(home)/page.tsx
+++ app/(landing)/(home)/page.tsx
@@ -1,8 +1,44 @@
+"use client";
+import { Button } from "@/components/ui/button";
 import React from "react";
+import { TypeAnimation } from "react-type-animation";
+
 export default function Home() {
   return (
-      <>
-        Home Page
-      </>
+    <div className="bg-primaryColor h-screen">
+      <section
+        key="hero"
+        className="bg-gradient h-[500px] flex flex-col items-center justify-center w-full"
+      >
+        <div className="items-center flex flex-col bg-white w-[95%] gap-y-10 py-10">
+          <div className="text-3xl text-center flex flex-col">
+            <h1>Your AI Companion for: </h1>
+            <span className="text-gradientFirst">
+              <TypeAnimation
+                sequence={[
+                  // Same substring at the start will only be typed out once, initially
+                  "Text!",
+                  1000, // wait 1s before replacing "Mice" with "Hamsters"
+                  "Images!",
+                  1000,
+                  "Code!",
+                  1000,
+                ]}
+                wrapper="span"
+                speed={50}
+                repeat={Infinity}
+              />
+            </span>
+          </div>
+          <div className=" flex flex-col gap-y-4 w-3/5">
+            <Button variant="ghost" size="lg" className="font-semibold">
+              Login
+            </Button>
+            <Button className="font-semibold">Get Started</Button>
+          </div>
+        </div>
+      </section>
+      <section key="body"></section>
+    </div>
   );
 }
