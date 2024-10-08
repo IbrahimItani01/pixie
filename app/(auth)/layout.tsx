956a7924636fd2adc8745fd03797c3990aebf12f
Ibrahim Itani <I.itani2002@gmail.com>
2024-10-08 19:37:44 +0300

    fix auth routing issue

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const AuthLayout=({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) => {
+    return (
+       <>
+       {children}
+       </>
+    );
+  }
+
+export default AuthLayout
